66d9fc3307a41cf914a62c9811e3080ef6aef889
treesrc <talattufekci@gmail.com>
2019-07-29 17:31:44 +0300

    packaging

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fs = window.require('fs');
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {files: []}
+  }
+
+  componentDidMount() {
+    fs.readdir('/', (err, files) => {
+        this.setState({files: files});
+    });
+  }  
+
+  renderFiles = () => {
+    return this.state.files.map((file, index) => {
+        return (<p key={index}>{file}</p>);
+    })
+  };
+
+  render() {
+      return (
+          <div className="App">
+              <div className="App-header">
+                  <img src={logo} className="App-logo" alt="logo"/>
+                  <h2>Welcome to React/NW.js</h2>
+              </div>
+              <p className="App-intro">
+                  Hello NW.js!
+              </p>
+              {this.renderFiles()}
+          </div>
+      );
+  }
 }
 
 export default App;
